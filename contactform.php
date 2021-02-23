a911c8dcb131e94bc99727c82a2b0f0c602a3861
Jovica Ickov <jovica.ickov@gmail.com>
2021-02-23 20:17:50 +0100

    Changed php contact form

--- contactform.php
+++ contactform.php
@@ -1,14 +1,16 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    $subject = "New e-mail";
+    $subject = "Pristigla vam je nova poruka sa sajta";
     $name = $_POST['name'];
     $emailFrom = $_POST['email'];
     $message = $_POST['message'];
+    $findus = $_POST['find-us'];
+    $phone = $_POST['tel'];
     
     $mailTo = "jovica.ickov@hotmail.com";
     $headers = "From: ".$emailFrom;
-    $txt = "You have received an e-mail from: ".$name.".\n\n"."Message: ".$message;
+    $txt = "Dobili ste novu poruku od: ".$name.".\n\n"."Broj telefona: ".$phone.".\n\n"."Poruka: ".$message."\n\n"."Saznao sam za vas preko: ".$findus.".";
     
     mail($mailTo, $subject, $txt, $headers);
     header("Location: index.html");
